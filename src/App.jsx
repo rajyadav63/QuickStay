92c66da83a14d58bfed89c946ea99eb1e3102517
rajyadav63 <vector36razz@gmail.com>
2025-06-15 00:08:54 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <h1 className='bg-blue-400'>QuickStay</h1>
+      <Navbar />
     </div>
   )
 }
